332f2595c71c098a58462cb9c2188c8dc17317b6
Syed Raza <syed.raza011@gmail.com>
2022-12-12 10:44:54 -0500

    Example

---
+++ index.js
@@ -0,0 +1,10 @@
+// //Class work
+/*
+
+pwd- directory or address
+~- means home folder
+mkdri fullstack
+creating a file write touvh 
+how to find in git hwere are you in git
+to see hidden ls -al it will show any hidden folders
+*/
